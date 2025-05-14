bd620f65b7ba80f643f9ef000393444983e4ec6a
Mok Ho <FMokHo@dot.nyc.gov>
2025-05-14 15:35:05 -0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app= express();
+
+app.get("/", (req,res)=>{
+    res.send("server is ready");
+})
+
+app.listen(5000,()=>{
+    console.log("Serve started at http://localhost:5000");
+});
+
+
+
