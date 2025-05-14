20fc3eafcd2f0ab3ea3a594895ae982fddbe07d1
freddycmh <fleddymok@gmail.com>
2025-05-14 16:00:40 -0400

    adding procuts database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from './config/db.js'
+
+dotenv.config();
 
 const app= express();
 
-app.get("/", (req,res)=>{
+app.get("/product", (req,res)=>{
     res.send("server is ready");
 })
 
+console.log(process.env.MONGO_URI);
+
 app.listen(5000,()=>{
+    connectDB();
     console.log("Serve started at http://localhost:5000");
 });
+
 
 
 
